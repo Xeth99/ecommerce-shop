14325fdec2ea0db092e4674ba0e6090111ef9629
xeth99 <emmanuelfemi646@gmail.com>
2025-07-25 09:51:33 +0100

    id params issue foxed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -18,12 +18,11 @@
 import { stripe } from "@/lib/stripe";
 import ProductDetailWrapper from "@/components/product_details_wrapper";
 
-type Props = {
-  params: { id: string };
-};
-
-export default async function ProductPage({ params }: Props) {
-  const product = await stripe.products.retrieve(params.id, {
+export default async function ProductPage({ params }: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params;
+  const product = await stripe.products.retrieve(id, {
     expand: ["default_price"],
   });
 
